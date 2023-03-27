b2247b790b30470efda981d3800af0bacea11c60
Gena Baslumbash <v.v.valiullin@yandex.ru>
2023-03-27 14:34:24 +0400

    Created contact page

--- Volgaida.Frontend/src/app/app.module.ts
+++ Volgaida.Frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
