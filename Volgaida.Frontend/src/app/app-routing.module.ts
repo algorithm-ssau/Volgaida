603ed04e5560630c0e9043badcf1dbe4f5ce09bd
Alex <ffgh04@gmail.com>
2023-03-28 01:49:31 +0400

    Added navigation

--- Volgaida.Frontend/src/app/app-routing.module.ts
+++ Volgaida.Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ContactPageComponent} from "./pages/contact-page/contact-page/contact-page.component";
+import {MenuPageComponent} from "./pages/menu-page/menu-page/menu-page.component";
+import {HomePageComponent} from "./pages/home-page/home-page/home-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'menu', component: MenuPageComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- Volgaida.Frontend/src/app/app.module.ts
+++ Volgaida.Frontend/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page/contact-page.component';
 import { MenuPageComponent } from './pages/menu-page/menu-page/menu-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ContactPageComponent,
-    MenuPageComponent
+    MenuPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
