6c98b990310325155f030cc0009bd5349310a2fb
V1lor <konshalex@gmail.com>
2023-04-23 16:00:17 +0400

    Renamed product tags

--- Volgaida.Backend/Model/ProductTag.py
+++ Volgaida.Backend/Model/ProductTag.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class ProductTag(Enum):
-    alcohol = 'alcohol'
-    soup = 'soup'
-    pizza = 'pizza'
-    pasta = 'pasta'
-    garnish = 'garnish'
-    meat = 'meat'
+    bar_card = 'Барная карта'
+    salad = 'Салат'
+    drink = 'Напиток'
+    main_dish = 'Горячее блюдо'
+    snack = 'Закуска'
+    dessert = 'Десерт'
