a3db089dbe064011004494d31510a682661b83fa
MaksimShchelokov <MAKS125X@yandex.ru>
2023-04-29 22:56:49 +0400

    Removed unnecessary requests, tables and queries have been redesigned in accordance with the new models

--- Volgaida.Backend/main.py
+++ Volgaida.Backend/main.py
@@ -5,42 +5,60 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 
-from Model.Product import Product
+from Dto.CategoryDto import CategoryDto
+from Dto.ProductFullDto import ProductFullDto
+from Dto.ProductShortDto import ProductShortDto
+
+# router = APIRouter(prefix='/api')
 
 app = FastAPI(
-    title='Valgoida'
+    title='Valgoida',
+    message='Hello Volgaida'
 )
+# app.include_router(router)
 
-products_db = [
-    {'id': 1, 'name': 'borsch', 'price': 200, 'tagList': [
-        'soup'
-    ]},
-    {'id': 2, 'name': 'steak', 'price': 300, 'tagList': [
-        'meat'
-    ]},
-    {'id': 3, 'name': 'barbecue', 'price': 250, 'tagList': [
-        'meat'
-    ]},
-    {'id': 4, 'name': 'french_fries', 'price': 105, 'tagList': [
-        'garnish'
-    ]}
+category_table = [
+    {'id': 1, 'name': 'Барная карта', 'image': '1112'},
+    {'id': 2, 'name': 'Салат', 'image': '1112'},
+    {'id': 3, 'name': 'Напиток', 'image': '1112'},
+    {'id': 4, 'name': 'Горячее блюдо', 'image': '1112'},
+    {'id': 5, 'name': 'Закуска', 'image': '1112'},
+    {'id': 6, 'name': 'Десерт', 'image': '1112'}
+]
+
+products_full_table = [
+    {'id': 1, 'name': 'borsch', 'image': '1112', 'description': 'борщ', 'price': 200},
+    {'id': 2, 'name': 'steak', 'image': '1112', 'description': 'стейк', 'price': 300},
+    {'id': 3, 'name': 'caesar_salad', 'image': '1112', 'description': 'салат цезарь', 'price': 250},
+    {'id': 4, 'name': 'french_fries', 'image': '1112', 'description': 'картофель фри', 'price': 105}
+]
+
+products_short_table = [
+    {'id': 1, 'name': 'borsch', 'image_compressed': '1112', 'category_id': 4},
+    {'id': 2, 'name': 'steak', 'image_compressed': '1112', 'category_id': 4},
+    {'id': 3, 'name': 'caesar_salad', 'image_compressed': '1112', 'category_id': 2},
+    {'id': 4, 'name': 'french_fries', 'image_compressed': '1112', 'category_id': 5}
 ]
 
 
 @app.exception_handler(ValidationError)
 async def validation_exception_handler(exc: ValidationError):
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({'detail': exc.errors()}),
+        status_code=status.HTTP_404_NOT_FOUND,
+        content=jsonable_encoder({'detail': 'Entity not found'})
     )
 
 
-@app.get('/products')
-def get_product(product_id: int = 1):
-    return [product for product in products_db if product.get('id') == product_id]
+@app.get('/categories', response_model=List[CategoryDto])
+def get_categories():
+    return [category for category in category_table]
 
 
-@app.post('/')
-def add_trades(products: List[Product]):
-    products_db.extend(products)
-    return {'status': 200, 'data': products_db}
+@app.get('/products/{product_id}', response_model=ProductFullDto)
+def get_product_by_id(product_id: int = 1):
+    return [product for product in products_full_table if product.get('id') == product_id][0]
+
+
+@app.get('/products/category/{category_id}', response_model=List[ProductShortDto])
+def get_products_by_category(category_id: int = 1):
+    return [product for product in products_short_table if product.get('category_id') == category_id]
