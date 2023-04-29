73674ff556ab44f138b663ea833fab847ee8752b
0ddN0se <echepf@gmail.com>
2023-04-29 23:08:10 +0400

    Add home welcome

--- Volgaida.Frontend/src/app/pages/home-page/home-page/home-page.component.ts
+++ Volgaida.Frontend/src/app/pages/home-page/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageSize} from "../../../core/ImageSize";
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  protected readonly ImageSize = ImageSize;
 
   constructor() { }
 
