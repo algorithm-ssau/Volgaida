9256a3cbc481e272e6effaed7a7c9121e87a06d9
Alex <ffgh04@gmail.com>
2023-04-29 22:22:11 +0400

    Create http services

---
+++ Volgaida.Frontend/src/app/core/http/http-product.service.ts
@@ -0,0 +1,39 @@
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {ProductFullDto} from "../dto/ProductFullDto";
+import {catchError, Observable, throwError} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {TuiAlertService} from "@taiga-ui/core";
+import {ProductShortDto} from "../dto/ProductShortDto";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpProductService {
+  private readonly url = environment.API_URL + 'product/';
+
+  constructor(private readonly http: HttpClient, @Inject(TuiAlertService) private readonly alerts: TuiAlertService) {
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.alerts
+      .open(error.message, {label: 'Error!'})
+      .subscribe();
+
+    return throwError(() => error.message)
+  }
+
+  getById(productId: number): Observable<ProductFullDto> {
+    return this.http.get<ProductFullDto>(this.url + productId)
+      .pipe(
+        catchError(this.errorHandler.bind(this))
+      )
+  }
+
+  getByCategories(categoryId: number): Observable<ProductShortDto[]> {
+    return this.http.get<ProductShortDto[]>(this.url + 'catigory/' + categoryId)
+      .pipe(
+        catchError(this.errorHandler.bind(this))
+      )
+  }
+}
