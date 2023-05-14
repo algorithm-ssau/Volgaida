29b8ad5acf5e93155a9c4596bbc053a5f041d883
V1lor <konshalex@gmail.com>
2023-05-15 02:16:31 +0400

    updated category.py file to sqlalchemy

--- Volgaida.Backend/Models/Category.py
+++ Volgaida.Backend/Models/Category.py
@@ -1,4 +1,11 @@
-class Category:
-    id: int
-    name: str
-    image: str
+from database import Base
+from sqlalchemy import String, Integer, Column, Text
+from sqlalchemy.orm import relationship
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    image = Column(Text)
+    products = relationship('Product', back_populates='category')
