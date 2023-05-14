17068eb50ff7bd48334f682b1245b18d71e106e1
V1lor <konshalex@gmail.com>
2023-05-15 02:16:04 +0400

    updated product.py file to sqlalchemy

--- Volgaida.Backend/Models/Product.py
+++ Volgaida.Backend/Models/Product.py
@@ -1,7 +1,15 @@
-class Product:
-    id: int
-    name: str
-    price: float
-    description: str
-    image: str
-    category_id: int
+from database import Base
+from sqlalchemy import String, Integer, Column, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    price = Column(Integer)
+    description = Column(Text)
+    image = Column(Text)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship('Category', back_populates='products')
+
