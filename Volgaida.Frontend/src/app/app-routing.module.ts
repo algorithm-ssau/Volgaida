d7efd77e4b3eda87374844b922e4de584922041b
0ddN0se <echepf@gmail.com>
2023-05-21 23:38:52 +0400

    Rename Contact to About

--- Volgaida.Frontend/src/app/app-routing.module.ts
+++ Volgaida.Frontend/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ContactPageComponent} from "./pages/contact-page/contact-page/contact-page.component";
+import {AboutPageComponent} from "./pages/contact-page/about-page/about-page.component";
 import {MenuPageComponent} from "./pages/menu-page/menu-page/menu-page.component";
 import {HomePageComponent} from "./pages/home-page/home-page/home-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'contact', component: ContactPageComponent},
+  {path: 'about', component: AboutPageComponent},
   {path: 'menu', component: MenuPageComponent, pathMatch: 'full'},
   {path: '**', redirectTo: '/home'}
 ];
--- Volgaida.Frontend/src/app/app.module.ts
+++ Volgaida.Frontend/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page/contact-page.component';
+import { AboutPageComponent } from './pages/contact-page/about-page/about-page.component';
 import { MenuPageComponent } from './pages/menu-page/menu-page/menu-page.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import {NgOptimizedImage} from "@angular/common";
@@ -20,7 +20,7 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
   declarations: [
     AppComponent,
     HomePageComponent,
-    ContactPageComponent,
+    AboutPageComponent,
     MenuPageComponent,
     NavigationComponent,
     FooterComponent,
