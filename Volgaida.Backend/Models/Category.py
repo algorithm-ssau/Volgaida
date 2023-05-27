b7da88949a5f9dd090b43541817475a4fa92503a
Alex <ffgh04@gmail.com>
2023-05-27 22:04:17 +0400

    Rework database model

--- Volgaida.Backend/Models/Category.py
+++ Volgaida.Backend/Models/Category.py
@@ -1,11 +1,12 @@
-from database import Base
-from sqlalchemy import String, Integer, Column, Text
+from sqlalchemy import String, Integer, Column, LargeBinary
 from sqlalchemy.orm import relationship
+
+from Models.base import Base
 
 
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False, unique=True)
-    image = Column(Text)
+    image = Column(LargeBinary)
     products = relationship('Product', back_populates='category')
--- Volgaida.Backend/Models/database.py
+++ Volgaida.Backend/Models/database.py
@@ -1,11 +1,58 @@
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from psycopg2 import connect, extensions
+
+from Models.base import Base
+from Product import Product
+from Category import Category
 
 
-DB_URL = "mysql://root:SuperSecretPassword@127.0.0.1:3306/volgaida"
-engine = create_engine(DB_URL, echo=True)
+class DatabaseServer:
+    DEFAULT_HOST = "localhost"
+    DEFAULT_PORT = 5432
+    DEFAULT_USERNAME = "postgres"
+    DEFAULT_PASSWORD = "postgres"
+    DEFAULT_DATABASE_NAME = "volgaida"
 
-Base = declarative_base()
+    DEFAULT_URL = f"postgresql://{DEFAULT_USERNAME}:{DEFAULT_PASSWORD}@{DEFAULT_HOST}:{DEFAULT_PORT}/{DEFAULT_DATABASE_NAME}"
 
-SessionLocal = sessionmaker(autocommit=False, bind=engine)
+    Engine = None
+    SessionLocal = None
+
+
+def create_database(host, port, user, password, database_name):
+    connection = connect(host=host, port=port, user=user, password=password)
+    connection.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cursor = connection.cursor()
+
+    cursor.execute(f"DROP DATABASE IF EXISTS {database_name};")
+    cursor.execute(f"CREATE DATABASE {database_name};")
+
+    cursor.close()
+    connection.close()
+
+
+def init_database_server():
+    DatabaseServer.Engine = create_engine(DatabaseServer.DEFAULT_URL, echo=True)
+    DatabaseServer.SessionLocal = sessionmaker(autocommit=False, bind=DatabaseServer.Engine)
+
+
+def create_tables():
+    Base.metadata.create_all(DatabaseServer.Engine)
+
+
+def main():
+    create_database(
+        DatabaseServer.DEFAULT_HOST,
+        DatabaseServer.DEFAULT_PORT,
+        DatabaseServer.DEFAULT_USERNAME,
+        DatabaseServer.DEFAULT_PASSWORD,
+        DatabaseServer.DEFAULT_DATABASE_NAME
+    )
+
+    init_database_server()
+    create_tables()
+
+
+main()
