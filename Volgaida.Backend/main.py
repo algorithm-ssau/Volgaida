006dc18e009941e50fdd659c517fe5f15559812b
MaksimShchelokov <MAKS125X@yandex.ru>
2023-05-28 22:29:25 +0400

    Added response for getting products by category id

--- Volgaida.Backend/main.py
+++ Volgaida.Backend/main.py
@@ -9,7 +9,7 @@ from Dto.CategoryDto import CategoryDto
 from Dto.ProductFullDto import ProductFullDto
 from Dto.ProductShortDto import ProductShortDto
 from Models.database import select_categories, select_category_image_by_id, select_full_product_by_id, \
-    select_short_products, select_product_image_by_id, select_category_by_id
+    select_short_products, select_product_image_by_id, select_category_by_id, select_short_products_by_category
 
 # uvicorn main:app --reload
 
@@ -52,6 +52,12 @@ def get_category_image_by_id(category_id: int = 1):
     return Response(content=category_image, media_type="image/jpg")
 
 
+@app.get('/category/{category_id}', response_model=str)
+def get_category_by_id(category_id: int = 1):
+    category = select_category_by_id(category_id)
+    return category
+
+
 @app.get('/products/{product_id}', response_model=ProductFullDto)
 def get_product_by_id(product_id: int = 1):
     products = select_full_product_by_id(product_id)
@@ -78,12 +84,15 @@ def get_products():
              }
          },
          response_class=Response)
-def get_category_image_by_id(product_id: int = 1):
+def get_product_image_by_id(product_id: int = 1):
     product_image = select_product_image_by_id(product_id)
     return Response(content=product_image, media_type="image/jpg")
 
 
-@app.get('/category/{category_id}', response_model=str)
-def get_category_by_id(category_id: int = 1):
-    category = select_category_by_id(category_id)
-    return category
+@app.get('/products/category/{category_id}', response_model=List[ProductShortDto])
+def get_products_by_category(category_id: int):
+    products = select_short_products_by_category(category_id)
+    products_dto = []
+    for product in products:
+        products_dto.append(ProductShortDto(id=product[0], name=product[1]))
+    return products_dto
