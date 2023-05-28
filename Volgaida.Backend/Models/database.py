fb7e8537862e242337f120fd2290c5fee20b1cfd
ElClown <caramba334@gmail.com>
2023-05-28 13:41:27 +0400

    Filled hot dishes

--- Volgaida.Backend/Models/database.py
+++ Volgaida.Backend/Models/database.py
@@ -45,10 +45,23 @@ def fill_database(host, port, user, password, database_name):
 
     cursor = connection.cursor()
     categories_names = ["Горячие блюда", "Десерты", "Закуски", "Салаты", "Барная карта", "Напитки"]
-    categories_photos = ["Паста карбонара", "Тирамису", "Куриный бутерброд на деревянной доске", "Салат с пряной говядиной", "Джонни Сильверхенд", "Шмель"]
-    for i in range(1,7):
+    categories_photos = ["Паста карбонара", "Тирамису", "Куриный бутерброд на деревянной доске",
+                         "Салат с пряной говядиной", "Джонни Сильверхенд", "Шмель"]
+    for i in range(1, 7):
         binary_image = open(get_image_file(categories_photos[i]), 'rb').read()
-        cursor.execute(f"INSERT INTO categories (id, name, image) VALUES(%s,%s,%s)",(i, categories_names[i],Binary(binary_image)))
+        cursor.execute(f"INSERT INTO categories (id, name, image) VALUES(%s,%s,%s)",
+                       (i, categories_names[i], Binary(binary_image)))
+
+    hot_dish_photos = ["Паста Карбонара", "Судак на гриле", "Мясо с тушеным картофелем", "Пицца Четыре сыра",
+                       "Пицца с ветчиной и грибами", "Пицца с куриной грудкой"]
+    hot_dish_comp = ["Спагетти, бекон, сливки, сыр пармезан, яйца, зелень", "Судак, лимон, зелень",
+                     "Говядина,томаты, морковь, лук репчатый", "пармезан, чеддер, дор блю, гауда",
+                     "томатный соус, ветчина, шампиньоны, сыр гауда",
+                     "томатный соус, куриная грудка, свежие томаты, сыр гауда"]
+    hot_dish_weight = [270, 350, 250, 350, 330, 380]
+    hot_dish_price = [355, 389, 359, 369, 349, 359]
+    hot_dish_pfc = ["13/13/31", "22/1/0", "20/3/25", "46/62/97", "41/35/116", "47/28/120"]
+
     cursor.close()
     connection.close()
 
